9fbcbd76229ee743e6125ff55cfe286bc525ac48
kk <z124157799@gmail.com>
2015-08-17 11:45:37 +0800

    eidt css url

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
     <title>Ragnarok Online-Moonlight Mio Sole&Luna</title>
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
 
 
     <!-- Custom CSS -->
